fe1d0251f21282285fac3c29b5ec5df826648f2c
Techie <149437995+FarRza111@users.noreply.github.com>
2023-12-04 12:09:28 -0800

    Update _OOP.py

--- _OOP.py
+++ _OOP.py
@@ -1,1 +1,33 @@
-.....
+# .....
+
+import csv
+import os
+import requests
+from http import HTTPStatus
+from fake_useragent import UserAgent
+
+class Web:
+
+    def __init__(self, csv_path):
+        self.csv_path = csv_path
+
+
+    def get_websites(self):
+
+        websites: list[str] = []
+        with open(self.csv_path, 'r') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                if 'https://' not in row[1]:
+                    websites.append(f'https://{row[1]}')
+                else:
+                    websites.append(row[1])
+
+        return websites
+
+
+
+cs = Web(os.path.join(os.getcwd(), 'websites.csv'))
+print(cs.get_websites())
+
+# print(get_websites(os.path.join(os.getcwd(), 'websites.csv')))
