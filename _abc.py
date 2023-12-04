21bef783807d1a6394057644cc1a9b664097fe67
Techie <149437995+FarRza111@users.noreply.github.com>
2023-12-04 11:25:06 -0800

    Create _abc.py

---
+++ _abc.py
@@ -0,0 +1,11 @@
+from abc import abstractmethod, ABC
+
+class ISWebscrape(ABC):
+
+    @abstractmethod
+    def method1(self):
+        pass
+
+    @abstractmethod
+    def method2(self):
+        pass
